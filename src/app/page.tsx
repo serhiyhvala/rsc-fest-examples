371936d8d0b0fb7cd393a48b85995ee42c4eb199
Sergiy Khvala <sergiy.bootcamp@macpaw.com>
2024-02-17 16:40:41 +0200

    feat: create container component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,50 @@
-"use client";
-
-import styled from "styled-components";
-
-const Container = styled.div`
-  max-width: 1300px;
-  width: 100%;
-  margin: 0 auto;
-`;
+import { Container } from "@/shared/ui";
 
 export default function HomePage() {
   return (
     <Container>
-      <span>Home page</span>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Eget felis eget nunc
+        lobortis mattis aliquam. Volutpat sed cras ornare arcu dui vivamus arcu
+        felis. Mauris rhoncus aenean vel elit. Quis blandit turpis cursus in hac
+        habitasse platea dictumst. Sagittis orci a scelerisque purus semper eget
+        duis at. Fringilla est ullamcorper eget nulla facilisi etiam dignissim.
+        Sed tempus urna et pharetra pharetra massa massa ultricies mi. Volutpat
+        blandit aliquam etiam erat velit scelerisque. Metus dictum at tempor
+        commodo ullamcorper a lacus vestibulum sed. In pellentesque massa
+        placerat duis ultricies. Condimentum lacinia quis vel eros. Risus nec
+        feugiat in fermentum posuere urna nec tincidunt praesent. Dictumst
+        vestibulum rhoncus est pellentesque elit. In eu mi bibendum neque
+        egestas congue quisque. Eu turpis egestas pretium aenean pharetra magna
+        ac. Diam phasellus vestibulum lorem sed. Sed tempus urna et pharetra
+        pharetra massa massa ultricies mi.
+      </span>
+      <span>
+        Id leo in vitae turpis massa sed elementum. Aliquam id diam maecenas
+        ultricies mi eget mauris pharetra et. Tristique senectus et netus et
+        malesuada fames ac. Ullamcorper velit sed ullamcorper morbi tincidunt
+        ornare massa eget. Feugiat sed lectus vestibulum mattis ullamcorper
+        velit sed. At tellus at urna condimentum. Ac felis donec et odio
+        pellentesque diam volutpat. Tristique risus nec feugiat in fermentum
+        posuere. Lacus sed turpis tincidunt id aliquet risus feugiat in ante.
+        Sagittis purus sit amet volutpat consequat mauris. Et magnis dis
+        parturient montes.
+      </span>
+      <span>
+        Quis auctor elit sed vulputate mi sit. Lacus sed turpis tincidunt id.
+        Eget aliquet nibh praesent tristique magna. Semper eget duis at tellus.
+        Ultricies integer quis auctor elit. Urna nunc id cursus metus aliquam
+        eleifend. Rhoncus est pellentesque elit ullamcorper dignissim cras.
+        Volutpat blandit aliquam etiam erat velit. Posuere lorem ipsum dolor sit
+        amet consectetur. Amet tellus cras adipiscing enim eu turpis egestas
+        pretium aenean. Etiam erat velit scelerisque in dictum non. Varius quam
+        quisque id diam vel quam elementum pulvinar etiam. Congue quisque
+        egestas diam in arcu. Non blandit massa enim nec dui nunc. Sed turpis
+        tincidunt id aliquet risus feugiat. Integer quis auctor elit sed
+        vulputate mi sit amet. Elit at imperdiet dui accumsan sit amet nulla
+        facilisi morbi.
+      </span>
     </Container>
   );
 }
---
+++ src/shared/ui/container.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1300px;
+  width: 100%;
+  margin: 0 auto;
+`;
