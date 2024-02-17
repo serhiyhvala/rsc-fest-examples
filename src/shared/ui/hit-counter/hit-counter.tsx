f6f36a9c94be126cc9df832b2e4b96b588f73195
Sergiy Khvala <sergiy.bootcamp@macpaw.com>
2024-02-17 15:40:03 +0200

    feat: update hit-counter component

--- src/shared/ui/hit-counter/hit-counter.tsx
+++ src/shared/ui/hit-counter/hit-counter.tsx
@@ -1,7 +1,11 @@
 import styles from "./hit-counter.module.scss";
 import { prisma } from "@/shared/db";
+import { useState } from "react";
+import clsx from "clsx";
 
 export const HitCounter = async () => {
+  const [censored, setCensored] = useState(true);
+
   const page = await prisma.page.update({
     where: {
       pageTitle: "First Page",
@@ -11,5 +15,12 @@ export const HitCounter = async () => {
     },
   });
 
-  return <button className={styles.button}>{page?.viewsCount}</button>;
+  return (
+    <button
+      onClick={() => setCensored(!censored)}
+      className={clsx(styles.button, { [styles.censored]: censored })}
+    >
+      {page?.viewsCount}
+    </button>
+  );
 };
