7682c64577e66511120667d2ccbcfe6d9a176c6c
Sergiy Khvala <sergiy.bootcamp@macpaw.com>
2024-02-18 00:53:15 +0200

    feat: update scripts and update clock component

--- src/shared/ui/clock.tsx
+++ src/shared/ui/clock.tsx
@@ -5,14 +5,21 @@ import { format } from "date-fns";
 
 export const Clock = () => {
   const [time, setTime] = useState(new Date());
+  const [time2, setTime2] = useState<Date>();
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date());
+      setTime2(new Date());
     }, 50);
 
     return () => clearInterval(interval);
   }, []);
 
-  return <p>{format(time, "hh:mm:ss.S a")}</p>;
+  return (
+    <div>
+      <p suppressHydrationWarning>{format(time, "hh:mm:ss.S a")}</p>
+      <p>{time2 ? format(time2, "hh:mm:ss.S a") : "---"}</p>
+    </div>
+  );
 };
