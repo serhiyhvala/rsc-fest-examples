abb441eefe1eb8b248a7ece3dbc3957831e99620
Sergiy Khvala <sergiy.bootcamp@macpaw.com>
2024-02-18 14:39:10 +0200

    feat: update styles for comment widget

--- src/widgets/comments/ui/comments.tsx
+++ src/widgets/comments/ui/comments.tsx
@@ -1,9 +1,19 @@
 import { prisma } from "@/shared/api";
 import { delay } from "@/shared/lib";
+import styles from "../styles/comments.module.scss";
 
 export const Comments = async () => {
   await delay(4000);
   const comments = await prisma.comments.findMany();
 
-  return JSON.stringify(comments);
+  return (
+    <div className={styles.container}>
+      {comments.map((item) => (
+        <div key={item.id} className={styles.comment}>
+          <span>{item.user_name}</span>
+          <p>{item.comment}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
